8c58ff084f27b98915e171862eeda8063d2f4341
Alexander Hazankin <103335228+AlexanderHazankin@users.noreply.github.com>
2022-12-16 22:36:20 +0200

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -1,30 +1,42 @@
 import random
 import string
 
+# import the ascii letters and numbers, as well as a list of symbols
 letters = string.ascii_letters
 numbers = list(map(str, range(0, 10)))
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+# print a welcome message
 print("Welcome to the PyPassword Generator!")
+
+# ask the user how many letters, numbers, and symbols they want in their password
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 
-random_list = []
+# create an empty list to store the randomly selected characters
+letter_list = []
 
-random_letter = random.choice(letters)
-random_number = random.choice(numbers)
-random_symbol = random.choice(symbols)
+# select a random letter, number, and symbol to start
+random_letter = nr_letters
+random_number = nr_numbers
+random_symbol = nr_symbols
 
+# add the desired number of random letters to the list
 for letter in range(nr_letters):
-    random_letter = random.choice(letters)
-    random_list.append(random_letter)
-for number in range(nr_numbers):
-    random_number = random.choice(numbers)
-    random_list.append(random_number)
-for symbol in range(nr_symbols):
-    random_symbol = random.choice(symbols)
-    random_list.append(random_symbol)
+    letter_list += random.choice(letters)
 
-random.shuffle(random_list)
-print(''.join(random_list))
+# add the desired number of random numbers to the list
+for number in range(nr_numbers):
+    letter_list += random.choice(numbers)
+
+# add the desired number of random symbols to the list
+for symbol in range(nr_symbols):
+    letter_list += random.choice(symbols)
+
+# shuffle the list of characters
+random.shuffle(letter_list)
+
+# convert the letter_list to a string and assign it to the password variable
+password = "".join(letter_list)
+print(password)
